214121f443336953f83b43bc6299f271acec3331
ripend06 <makoto6760@gmail.com>
2024-01-07 10:30:57 +0900

    feat:definition-prisma_230107

--- app/components/lib/next-auth/options.ts
+++ app/components/lib/next-auth/options.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import prisma from "../prisma";
 
 
 export const NExtAuthOptions: NextAuthOptions = {
